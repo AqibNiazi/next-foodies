64beb1227ebb41c92c7e5f1202e7a4b06bf8e20e
AqibNiazi <aqibjaved5201@gmail.com>
2025-09-16 23:32:15 +0500

    Main Header added to the application

---
+++ components/header/main-header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
+        <Image src={logoImg} alt="A plate with food on it." priority />
+        Foodies
+      </Link>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
