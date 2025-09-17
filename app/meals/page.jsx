314698e90f1c9fbd9a1553b0a3948fafbc24232a
AqibNiazi <aqibjaved5201@gmail.com>
2025-09-17 16:21:58 +0500

    Local database created with meals page setup completed

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
 const MealsPage = () => {
-  return <div>MealsPage</div>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link className="/meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
