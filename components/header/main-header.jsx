1b83e24fdfbf7c0c73771a344f8ee1bfa26bb8f6
AqibNiazi <aqibjaved5201@gmail.com>
2025-09-17 15:07:09 +0500

    Images Slider and community page completed

--- components/header/main-header.jsx
+++ components/header/main-header.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
+import NavLink from "./nav-link";
 const MainHeader = () => {
   return (
     <header className={classes.header}>
@@ -13,10 +14,10 @@ const MainHeader = () => {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/meals">Browse Meals</Link>
+            <NavLink href="/meals">Browse Meals</NavLink>
           </li>
           <li>
-            <Link href="/community">Foodies Community</Link>
+            <NavLink href="/community">Foodies Community</NavLink>
           </li>
         </ul>
       </nav>
