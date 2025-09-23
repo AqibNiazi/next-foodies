bd3880e2446c4ef738fd5f06540dcef73614908f
AqibNiazi <aqibjaved5201@gmail.com>
2025-09-23 12:00:14 +0500

    Form errors handled with useActionState hook

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,8 +1,11 @@
+"use client";
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 import MealFormSubmit from "@/components/meals/meals-form-submit";
+import { useActionState } from "react";
 export default function ShareMealPage() {
+  const [state, formAction] = useActionState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +15,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -40,6 +43,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
+          {state.message && <p className="error">{state.message}</p>}
           <ImagePicker label="Meal Image" id="image" name="image" />
           <p className={classes.actions}>
             <MealFormSubmit />
